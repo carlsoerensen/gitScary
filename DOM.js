d70898c65503edb9752eb7df906cbfc13bb268f0
carlsrensen <carlsrensen@gmail.com>
2023-08-24 10:52:54 +0200

    asd

--- DOM.js
+++ DOM.js
@@ -1,4 +1,15 @@
-const info = document.querySelector('h1');
+const info = document.querySelector('h2');
 const spanElement = document.createElement('span');
-spanElement.textContent = ' - New Content';
-h1.appendChild(spanElement);
+spanElement.textContent = ' - New Content hhaha ADAM';
+info.appendChild(spanElement);
+
+
+const colors = ['red', 'green', 'blue', 'orange', 'purple'];
+let currentColorIndex = 0;
+
+info.addEventListener('click', changeColor);
+
+function changeColor() {
+  info.style.color = colors[currentColorIndex];
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
+}
