07417865b1bd441e22161907d7ec047f301ac0e2
carlsrensen <carlsrensen@gmail.com>
2023-08-24 10:58:30 +0200

    asd

--- DOM.js
+++ DOM.js
@@ -6,10 +6,14 @@ info.appendChild(spanElement);
 
 const colors = ['red', 'green', 'blue', 'orange', 'purple'];
 let currentColorIndex = 0;
+let fontSize = 16;
 
-info.addEventListener('click', changeColor);
+info.addEventListener('click', changeAppearance);
 
-function changeColor() {
+function changeAppearance() {
   info.style.color = colors[currentColorIndex];
   currentColorIndex = (currentColorIndex + 1) % colors.length;
+
+  fontSize += 2;
+  info.style.fontSize = `${fontSize}px`;
 }
